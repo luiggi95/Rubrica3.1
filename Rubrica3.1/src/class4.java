2e11889eb16db028eaeaedf5fa2488df083af298
luiggi95 <luiggireyes1995@hotmail.com>
2016-08-26 09:56:19 -0500

    mi cuarto commit

---
+++ Rubrica3.1/src/class4.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author luiggifernando
+ */
+public class class4 {
+    // aprendiendo a utilizar GitHub
+    
+}
